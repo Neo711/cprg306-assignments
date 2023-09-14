43492b811db45019a2d1cceaf2ff377da29a87e8
Neo711 <111005592+Neo711@users.noreply.github.com>
2023-09-14 16:43:48 -0600

    moved files into week3

--- app/week3/page.js
+++ app/week3/page.js
@@ -3,7 +3,8 @@ import Link from "next/link";
 
 export default function Home() {
   return (<main>
-    <h1>CPRG-306 Demos</h1>
+    <title>CPRG-306: Web Development 2 Assignments</title>
+
     <Link href="/week2">Week 2 </Link>
     </main>
 
