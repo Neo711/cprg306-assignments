f6957127c26e99f40096d5322d1ec10c7a58433e
Neo711 <111005592+Neo711@users.noreply.github.com>
2023-09-14 16:47:12 -0600

    s

--- app/week3/page.js
+++ app/week3/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (<main>
-    <title>CPRG-306: Web Development 2 Assignments</title>
+    <title>CPRG-306: Web Development 2 Assignmentss</title>
 
     <Link href="/week2">Week 2 </Link>
     </main>
