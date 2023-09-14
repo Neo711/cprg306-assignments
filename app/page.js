8b701faa806de38dd46b6474f92d4fc135a3320c
Neo711 <111005592+Neo711@users.noreply.github.com>
2023-09-14 16:23:13 -0600

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+
+export default function Home() {
+  return (<main>
+    <h1>CPRG-306 Demos</h1>
+    <Link href="/week2">Week 2 </Link>
+    </main>
+
+  );
+}
---
+++ app/week2/event-list.js
@@ -0,0 +1,10 @@
+export default function EventList() {
+    return (
+    <div>
+        <h1>Event List</h1>
+        <li>Community Picnic</li>
+        <li>Date: 2023-09-15</li>
+        <li>Location: Downtown</li>
+    </div>
+    );
+}
