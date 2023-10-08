f68c570c5a9af3f71ae687b562ca75d748b6550a
Neo711 <cyrildizon04@gmail.com>
2023-10-08 09:53:08 -0600

    lol

--- app/week4/page.js
+++ app/week4/page.js
@@ -1,1 +1,26 @@
-//
+import { useState } from 'react';
+
+function component()  {
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState(1);
+    const [category, setCategory] = useState("produce");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        const item = {
+            name: name,
+            quantity: quantity,
+            category: category
+        };
+
+        console.log(item);
+    };
+
+    return(
+        <form onSubmit={handleSubmit}>
+            <button type="Submit">Submit</button>
+        </form>
+
+    );
+}
